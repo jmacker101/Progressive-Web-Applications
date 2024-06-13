48ff11eb36d9745b6818f1bdeb4b874f9b68298e
jmacker101 <jm470t@gmail.com>
2024-06-13 00:39:31 -0400

    set attribute?

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -3,6 +3,7 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    // is this a correct way to put it  must ask next time?
     event.preventDefault();
     butInstall.style.visibility = 'visible';
     butInstall.textContent = 'Install!'
@@ -10,7 +11,8 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
 });
 
 // TODO: Add an handler for the `appinstalled` event
