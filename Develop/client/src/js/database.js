04d5e460164cf3d7de3f31e452445dac32a7ad95
jmacker101 <jm470t@gmail.com>
2024-06-12 23:46:54 -0400

    add two console.logs at the end for testing

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -29,11 +29,11 @@ console.log("🚀 - data saved to the database", result);
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   // console.error('getDb not implemented');
-  // Database and version
+  // Database 
   const jateDB = await openDB("jate", 1);
   // New transaction specifying db and privileges
   const tx = jateDB.transaction("jate", "readonly");
-  // Open desired object store
+  // desired object 
   const store = tx.objectStore("jate");
   // Get all request
   const request = store.getAll();
