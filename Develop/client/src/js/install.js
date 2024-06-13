c1ba9652523b334bbf868a0ddf93e112feb0c36c
jmacker101 <jm470t@gmail.com>
2024-06-13 00:43:04 -0400

    making changes to install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -11,11 +11,13 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    // why is it setAttribute like this?
     butInstall.setAttribute('disabled', true);
     butInstall.textContent = 'Installed!';
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-
+    console.log('appinstalled', event);
+    // maybe testing out this console.log
 });
